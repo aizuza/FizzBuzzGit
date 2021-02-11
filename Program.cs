3e6a2967e33956e672f0d74b304d23153de543f9
Carlos <cjrfls@gmail.com>
2021-02-11 11:12:39 -0700

    se desarrollo el fizzbuzz

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace FizzBuzzGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 1 ; i <= 100 ; i++)
+            {
+                if(i % 3 == 0 & i % 5 == 0)
+                  {
+                      Console.WriteLine("FizzBuzz");
+                  }
+                  else if(i % 3 == 0)
+                 {
+                     Console.WriteLine("fizz");
+                 }
+                  else if(i % 5 == 0)
+                  {
+                      Console.WriteLine("Buzz");
+                  }
+                  
+                  else{
+                      Console.WriteLine(i);
+                  }
+            
+            }
         }
     }
 }
